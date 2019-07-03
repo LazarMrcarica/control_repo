eb72eab4f06723691fc9e687438bd3844bea02f9
LazarMrcarica <51820989+LazarMrcarica@users.noreply.github.com>
2019-07-03 14:51:02 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDOzoLfQvBVqg2gOrTrfuOJ2VKyOCrQIRkBBd9kzt5/qIr1KwFYyBCF5e0saKK3ukcJszzCIsCaLgbQU5w+zfAfsa59hZgIWUKV7yu0QAb+i4NvaUYDIAul0Uwsm9Ce66cTo8yelM/Zl7GigNSg6WvgvOtT2xC0UF19Js2RDg6ffODgJRl9MlfmwgRc3zgdzrSqSU2WTaTBWrCKVvpmTQJFT86zeVAEJCUE+jYV27emCC5NtpXZCM9Wa2APXFT3HM9PN+CnGTxiHNBbgoyaKR3jGXPERd8/OEAtMO5WIccr4qD0mMzzjJX4FkuVoyhsjgUHFEajEWKlguEc5/a5gw15',
 	}  
 }
